ffc4e6ff2165a0c3418a93dd41b488670a0961cd
Adam <adam.lj.haglund@gmail.com>
2019-09-11 21:28:52 +0200

    update index route

--- src/app.ts
+++ src/app.ts
@@ -11,18 +11,40 @@ App.use(BodyParser())
 
 router.get('/', (ctx: Context) => {
   ctx.body = `
-Usage:
-GET /api/ids?source={anilist|anidb|myanimelist|kitsu}&id={number}
+<pre>
+<b>GET /api/ids</b>
 
-Returns:
-{
-  anilist?: number
-  anidb?: number
-  myanimelist?: number
-  kitsu?: number
+enum Source {
+  anilist,
+  anidb,
+  myanimelist,
+  kitsu,
 }
 
+<b>Either use query parameters:</b>
+?source={Source}&id={number}
+
+<b>or send the query as a JSON body:</b>
+
+{ "anilist": 1337 }
+
+[{ "anilist": 1337, "anilist": 69, "anidb": 420 }]
+
+interface Entry {
+  anilist: number | null
+  anidb: number | null
+  myanimelist: number | null
+  kitsu: number | null
+}
+
+{ "anilist": 1337 } => Entry | null
+[{ ... }] => (Entry | null)[]
+
+<b>The response code will always be 200 (OK) or 204 (No content).
+If an entry is not found null is returned instead.</b>
+
 Source code is available on GitHub at https://github.com/BeeeQueue/arm-server
+</pre>
 `
 })
 
