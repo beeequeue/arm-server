1e7f684859a659b8ad6868bac61053b7de7ac131
Adam <adam@haglund.dev>
2021-06-01 22:29:36 +0200

    add response schema

--- src/routes/ids.ts
+++ src/routes/ids.ts
@@ -1,11 +1,13 @@
 import { FastifyPluginAsync, FastifyRequest } from "fastify"
 
+import { Relation } from "@/db"
 import { bodyHandler, bodyInputSchema, BodyQuery } from "@/schemas/json-body"
 import {
   handleQueryParams,
   queryInputSchema,
   QueryParamQuery,
 } from "@/schemas/query-params"
+import { responseBodySchema } from "@/schemas/response"
 import { isEmpty } from "@/utils"
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -13,18 +15,42 @@ type BodyInput = { Body: BodyQuery; Querystring: undefined }
 // eslint-disable-next-line @typescript-eslint/naming-convention
 type QueryInput = { Body: undefined; Querystring: QueryParamQuery }
 
-const handler = async (request: FastifyRequest<BodyInput | QueryInput>) => {
+const handler = async (
+  request: FastifyRequest<BodyInput | QueryInput>,
+): Promise<Relation | Relation[] | null> => {
   const isBodyQuery = isEmpty(request.query)
 
   if (isBodyQuery) {
-    return await bodyHandler(request.body!)
+    return (await bodyHandler(request.body!)) ?? null
   }
 
-  return await handleQueryParams(request.query!)
+  return (await handleQueryParams(request.query!)) ?? null
 }
 
 export const apiPlugin: FastifyPluginAsync = async (fastify) => {
-  fastify.get<QueryInput>("/ids", { schema: { querystring: queryInputSchema } }, handler)
+  fastify.get<QueryInput>(
+    "/ids",
+    {
+      schema: {
+        querystring: queryInputSchema,
+        response: {
+          200: responseBodySchema,
+        },
+      },
+    },
+    handler,
+  )
 
-  fastify.post<BodyInput>("/ids", { schema: { body: bodyInputSchema } }, handler)
+  fastify.post<BodyInput>(
+    "/ids",
+    {
+      schema: {
+        body: bodyInputSchema,
+        response: {
+          200: responseBodySchema,
+        },
+      },
+    },
+    handler,
+  )
 }
