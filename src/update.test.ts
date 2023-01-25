18b262269b579e285f6698b98f8b552fe82fdd7c
Adam <adam@haglund.dev>
2023-01-25 20:15:52 +0100

    fix fetching data

---
+++ src/update.test.ts
@@ -0,0 +1,91 @@
+import { $fetch } from "ohmyfetch/node"
+import { groupBy } from "remeda"
+import { afterAll, afterEach, expect, it, vi } from "vitest"
+
+import { knex, Relation } from "@/db"
+import {
+  AnimeListsSchema,
+  formatEntry,
+  removeDuplicates,
+  updateRelations,
+} from "@/update"
+
+declare const fetch: (url: string) => Promise<any>
+
+vi.mock("ohmyfetch/node")
+
+afterEach(async () => {
+  vi.resetAllMocks()
+  await knex.delete().from("relations")
+})
+
+afterAll(async () => {
+  await Promise.all([knex.destroy()])
+})
+
+const mockedFetch = vi.mocked($fetch)
+
+it("handles bad values", async () => {
+  mockedFetch.mockResolvedValue([
+    { anidb_id: 1337, themoviedb_id: "unknown" },
+    { anidb_id: 1338, thetvdb_id: "unknown" as any },
+    { anidb_id: 1339, imdb_id: "tt1337,tt1338,tt1339" },
+    { anidb_id: 1340, themoviedb_id: "unknown" },
+    { anidb_id: 1341, themoviedb_id: 1341 },
+  ] satisfies AnimeListsSchema)
+
+  await updateRelations()
+
+  await expect(knex.from("relations").select(["anidb", "imdb", "themoviedb", "thetvdb"]))
+    .resolves.toMatchInlineSnapshot(`
+    [
+      {
+        "anidb": 1337,
+        "imdb": null,
+        "themoviedb": null,
+        "thetvdb": null,
+      },
+      {
+        "anidb": 1338,
+        "imdb": null,
+        "themoviedb": null,
+        "thetvdb": null,
+      },
+      {
+        "anidb": 1339,
+        "imdb": null,
+        "themoviedb": null,
+        "thetvdb": null,
+      },
+      {
+        "anidb": 1340,
+        "imdb": null,
+        "themoviedb": null,
+        "thetvdb": null,
+      },
+      {
+        "anidb": 1341,
+        "imdb": null,
+        "themoviedb": 1341,
+        "thetvdb": null,
+      },
+    ]
+  `)
+})
+
+it.only("handles duplicates", async () => {
+  const entries: Relation[] = await fetch(
+    "https://raw.githubusercontent.com/Fribb/anime-lists/master/anime-list-full.json",
+  )
+    .then((r) => r.json())
+    .then((e) => e.map(formatEntry))
+
+  const groups = groupBy(removeDuplicates(entries), (e) => e.imdb!)
+  expect(
+    Object.fromEntries(
+      Object.entries(groups)
+        .filter(([id, g]) => id !== "undefined" && g.length > 1)
+        .map(([id, g]) => [id, g.length]),
+    ),
+  ).toStrictEqual({})
+})
--- src/update.ts
+++ src/update.ts
@@ -4,13 +4,13 @@ import { captureException } from "@sentry/node"
 
 import { logger } from "@/lib/logger"
 
-import { knex, Relation } from "./db"
+import { knex, Relation, Source } from "./db"
 import { updateBasedOnManualRules } from "./manual-rules"
 
 const isFetchError = <T>(response: T | FetchError): response is FetchError =>
   (response as FetchError).stack != null
 
-type AnimeListsSchema = Array<{
+export type AnimeListsSchema = Array<{
   anidb_id?: number
   anilist_id?: number
   "anime-planet_id"?: string
@@ -46,19 +46,52 @@ const fetchDatabase = async (): Promise<AnimeListsSchema | null> => {
   return response
 }
 
-const formatEntry = (entry: AnimeListsSchema[number]): Relation => ({
-  anidb: entry["anidb_id"],
-  anilist: entry["anilist_id"],
-  "anime-planet": entry["anime-planet_id"],
-  anisearch: entry["anisearch_id"],
-  imdb: entry["imdb_id"],
-  kitsu: entry["kitsu_id"],
-  livechart: entry["livechart_id"],
-  myanimelist: entry["mal_id"],
-  "notify-moe": entry["notify.moe_id"],
-  themoviedb:
-    typeof entry["themoviedb_id"] !== "string" ? entry["themoviedb_id"] : undefined,
-  thetvdb: entry["thetvdb_id"],
+const badValues = ["unknown", "tv special"] as const
+
+const handleBadValues = <T extends string | number | undefined>(
+  value: T | "unknown",
+): T | undefined => {
+  if (
+    typeof value === "string" &&
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    (badValues.includes(value as any) || value.includes(","))
+  ) {
+    return undefined
+  }
+
+  return value as T
+}
+
+export const removeDuplicates = (entries: Relation[]): Relation[] => {
+  const sources = Object.values(Source) as Source[]
+  const existing = new Map<Source, Set<unknown>>(sources.map((name) => [name, new Set()]))
+
+  return entries.filter((entry) => {
+    for (const source of sources) {
+      if (entry[source] == null) continue
+
+      const set = existing.get(source)!
+      if (set.has(entry[source])) return false
+
+      set.add(entry[source])
+    }
+
+    return true
+  })
+}
+
+export const formatEntry = (entry: AnimeListsSchema[number]): Relation => ({
+  anidb: handleBadValues(entry["anidb_id"]),
+  anilist: handleBadValues(entry["anilist_id"]),
+  "anime-planet": handleBadValues(entry["anime-planet_id"]),
+  anisearch: handleBadValues(entry["anisearch_id"]),
+  imdb: handleBadValues(entry["imdb_id"]),
+  kitsu: handleBadValues(entry["kitsu_id"]),
+  livechart: handleBadValues(entry["livechart_id"]),
+  myanimelist: handleBadValues(entry["mal_id"]),
+  "notify-moe": handleBadValues(entry["notify.moe_id"]),
+  themoviedb: handleBadValues(entry["themoviedb_id"]),
+  thetvdb: handleBadValues(entry["thetvdb_id"]),
 })
 
 export const updateRelations = async () => {
@@ -77,7 +110,11 @@ export const updateRelations = async () => {
   const formattedEntries = data
     .map(formatEntry)
     .filter((entry) => Object.values(entry).some((value) => value != null))
-  logger.info({ amount: formattedEntries.length }, `Formatted entries.`)
+  logger.info({ remaining: formattedEntries.length }, `Formatted entries.`)
+
+  logger.info(`Removing duplicates.`)
+  const goodEntries = removeDuplicates(formattedEntries)
+  logger.info({ remaining: goodEntries.length }, `Removed duplicates.`)
 
   logger.info("Updating database...")
   await knex.transaction((trx) =>
@@ -86,7 +123,7 @@ export const updateRelations = async () => {
       .from("relations")
       .transacting(trx)
       .then(async () => {
-        await knex.batchInsert("relations", formattedEntries, 100).transacting(trx)
+        await knex.batchInsert("relations", goodEntries, 100).transacting(trx)
       }),
   )
   logger.info("Updated database.")
