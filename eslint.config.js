2e2de8f57143405c3835077dab086b341ae87d13
Adam <adam@haglund.dev>
2024-10-13 05:00:41 +0200

    up deps, fix formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,37 @@
 import antfu from "@antfu/eslint-config"
 
 export default antfu({
-  ignores: ["**/package.json"],
-  stylistic: false,
-  test: { overrides: { "test/no-import-node-test": "off" } },
-  typescript: {
-    tsconfigPath: "tsconfig.json",
-    overrides: {
-      "no-console": "off",
-      "ts/no-use-before-define": "off",
-      "ts/consistent-type-definitions": "off",
-      "ts/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
-      "ts/no-unsafe-argument": "off",
-      "ts/no-unsafe-assignment": "off",
+	ignores: ["**/package.json"],
+	stylistic: false,
+	test: { overrides: { "test/no-import-node-test": "off" } },
+	typescript: {
+		tsconfigPath: "tsconfig.json",
+		overrides: {
+			"no-console": "off",
+			"ts/no-use-before-define": "off",
+			"ts/consistent-type-definitions": "off",
+			"ts/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
+			"ts/no-unsafe-argument": "off",
+			"ts/no-unsafe-assignment": "off",
 
-      "perfectionist/sort-imports": [
-        "error",
-        {
-          type: "natural",
-          internalPattern: ["@/**", "~/**"],
-          newlinesBetween: "always",
-          groups: [
-            ["builtin", "builtin-type"],
-            ["external", "external-type"],
-            ["internal", "internal-type"],
-            ["parent", "parent-type"],
-            ["sibling", "sibling-type"],
-            ["index", "index-type"],
-            "object",
-            "unknown",
-          ],
-        },
-      ],
-    },
-  },
+			"perfectionist/sort-imports": [
+				"error",
+				{
+					type: "natural",
+					internalPattern: ["@/**", "~/**"],
+					newlinesBetween: "always",
+					groups: [
+						["builtin", "builtin-type"],
+						["external", "external-type"],
+						["internal", "internal-type"],
+						["parent", "parent-type"],
+						["sibling", "sibling-type"],
+						["index", "index-type"],
+						"object",
+						"unknown",
+					],
+				},
+			],
+		},
+	},
 })
